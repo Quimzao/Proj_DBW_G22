0d205a8bc7ff0fb6122072372593770416b9cfcd
Joaquim Paulo Freitas Ferreira <117274700+Quimzao@users.noreply.github.com>
2025-03-11 10:26:58 +0000

    more stuff

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 import path from 'path'; 
 import { fileURLToPath } from 'url'; 
+import mainRoutes from './routes/mainRoutes.js';
 import loginRoutes from './routes/loginRoutes.js';
 import signupRoutes from './routes/signupRoutes.js';
 
@@ -12,8 +13,9 @@ const __dirname = path.dirname(__filename);
 app.set("view engine", "ejs"); //método para configurar a nossa view engine para “ejs”
 app.use(express.static(__dirname + "/public")); //é uma função middleware no framework Express.js para Node.js que serve arquivos estáticos, como imagens, arquivos CSS e JavaScript.
 app.use(express.urlencoded({ extended: true })); //é uma função middleware do Express.js que é usada para analisar dados de formulários HTML que são enviados para o servidor.
-app.use("/", loginRoutes); //é uma função middleware do Express.js que é usada para analisar dados de formulários HTML que são enviados para o servidor.
+app.use("/", mainRoutes); //é uma função middleware do Express.js que é usada para analisar dados de formulários HTML que são enviados para o servidor.
 app.use("/signup", signupRoutes); //é uma função middleware do Express.js que é usada para analisar dados de formulários HTML que são enviados para o servidor.
+app.use("/login", loginRoutes); //é uma função middleware do Express.js que é usada para analisar dados de formulários HTML que são enviados para o servidor.
 
 app.listen(3000, (err) => {
     if (err)
