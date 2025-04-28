e8b74ab21cab5312fb48aaf2473dc788ac1f7db7
Joaquim Paulo Freitas Ferreira <117274700+Quimzao@users.noreply.github.com>
2025-04-28 08:55:17 +0000

    metendo partials do footer e configurando profile um pedaco

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { getProfile, updateProfile } from "../controllers/profileController.js";
-import ensureAuthenticated from "../middleware/loggedIn.js";
+import isAuthenticated from "../middleware/loggedIn.js";
 
 const router = express.Router();
 
 // Route to display the profile
-router.get("/", ensureAuthenticated, getProfile);
+router.get("/", isAuthenticated, getProfile);
 
 // Route to update the profile
-router.post("/update", ensureAuthenticated, updateProfile);
+router.post("/update", isAuthenticated, updateProfile);
 
 export default router;
