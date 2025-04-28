97112546c3baa0f7c0a3768b45c3d48fd2c0d9b7
Joaquim Paulo Freitas Ferreira <117274700+Quimzao@users.noreply.github.com>
2025-04-28 21:07:21 +0000

    pfp selector

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -37,7 +37,15 @@ const DEFAULT_AVATARS = [
     '/images/avatars/avatar1.png',
     '/images/avatars/avatar2.png',
     '/images/avatars/avatar3.png',
-    '/images/avatars/avatar4.png'
+    '/images/avatars/avatar4.png',
+    '/images/avatars/avatar5.png',
+    '/images/avatars/avatar6.png',
+    '/images/avatars/avatar7.png',
+    '/images/avatars/avatar8.png',
+    '/images/avatars/avatar9.png',
+    '/images/avatars/avatar10.png',
+    '/images/avatars/avatar11.png',
+    '/images/avatars/avatar12.png'
 ];
 
 export const getProfile = (req, res) => {
